fa6e35b16e9eae07963e05a171f610ba117eb7ba
Kakinotane0815 <kakinotane-0815@outlook.jp>
2023-08-05 11:53:33 +0900

    一旦セーブ

--- Assets/Scripts/Effects/SampleEffect.cs
+++ Assets/Scripts/Effects/SampleEffect.cs
@@ -21,11 +21,13 @@ public class SampleEffect : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A))
         {
+            //座標の記憶
             Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             animalInfo.Enqueue(pos);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
+            //演出処理開始
             Debug.Log("StartEffect");
             StartCoroutine(ActiveEffect());
         }
@@ -36,18 +38,22 @@ public class SampleEffect : MonoBehaviour
         effects[effectCurrent].Play(pos);
         // 次のエフェクト再生する準備
         effectCurrent++;
+        //エフェクトの呼び出しが一通り終わったらカウントを0にする
         if (effects.Length == effectCurrent) effectCurrent = 0;
     }
 
     private IEnumerator ActiveEffect()
     {
-        isEffect = true;  
+        //エフェクト再生中の判定にする
+        isEffect = true;
+        //Queueの中身の要素数が0ではない限り処理を行う
         while (animalInfo.Count != 0)
         {
             //エフェクト再生
             Effect(animalInfo.Dequeue());
             yield return new WaitForSeconds(0.1f);
         }
+        //処理が終了したらエフェクト再生を終了する
         isEffect = false;
     }
 }
